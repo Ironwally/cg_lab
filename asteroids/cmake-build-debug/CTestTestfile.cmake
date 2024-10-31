1455362ec886e8f0c0474e864bd9c71fbb6b2922
Colin Bankart <blackrat@localhost.localdomain>
2024-10-31 12:02:25 +0100

    init

---
+++ asteroids/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/blackrat/Desktop/arbeit/Computergrafik_Labor/aufgabe_1/asteroids
+# Build directory: /home/blackrat/Desktop/arbeit/Computergrafik_Labor/aufgabe_1/asteroids/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ asteroids/colors.h
@@ -0,0 +1,6 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+inline extern Uint8 colors[] = {0x5F, 0x5F, 0x5, 0x5F};
+
+#endif
